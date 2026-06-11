ba930f1c5c124f3a0478169f1e1e89521179b80d
abak-sale <abaksale2@gmail.com>
2026-06-11 11:33:35 +0700

    menanbahkanhalaman tentang masih pakai function dan lewat HttpResponse belum pakai html

--- halaman/urls.py
+++ halaman/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.beranda, name='beranda')
+    path('',views.beranda, name='beranda'),
+    path('tentang/', views.tentang, name ='tentang')
 ]
--- halaman/views.py
+++ halaman/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def beranda(request):
     return HttpResponse("Halo ini kita pakai fungsion")
+
+def tentang(request):
+    return HttpResponse("Ini halaman tentang masih pakai Function")
